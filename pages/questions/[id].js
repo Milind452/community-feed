53e05feb9f7b04c2b6527a5d9b42814cfd89f18f
Milind452 <mohapatra.milind@gmail.com>
2023-02-21 11:43:03 +0530

    Fetch particular question and display

--- pages/questions/[id].js
+++ pages/questions/[id].js
@@ -1,13 +1,44 @@
 import { useRouter } from "next/router";
 
+import { useState, useEffect } from "react";
+
 import { QuestionDetailContainer } from "./questions.styles";
+import Card from "../../components/Card/Card";
 
 export default function QuestionDetail() {
     const router = useRouter();
     const { id } = router.query;
+
+    const [loading, setLoading] = useState(false);
+    const [question, setQuestion] = useState({});
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetch(
+                `https://api.stackexchange.com/2.2/questions/${id}?site=stackoverflow`
+            );
+            const result = await data.json();
+            if (result) {
+                setQuestion(result.items[0]);
+                setLoading(false);
+            }
+        }
+        setLoading(true);
+        id && fetchData();
+    }, [id]);
+
     return (
         <QuestionDetailContainer>
             <h2>Question: {id}</h2>
+            {loading ? (
+                <span>loading...</span>
+            ) : (
+                <Card
+                    title={question.title}
+                    views={question.view_count}
+                    answers={question.answer_count}
+                />
+            )}
         </QuestionDetailContainer>
     );
 }
