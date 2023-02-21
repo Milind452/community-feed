cbae86d7f6e63836f70a25f7cef10eb22850c5bc
Milind452 <mohapatra.milind@gmail.com>
2023-02-21 09:28:06 +0530

    Add questions route and component

---
+++ pages/questions/index.js
@@ -0,0 +1,9 @@
+import { QuestionsContainer } from "./questions.styles";
+
+export default function Questions() {
+    return (
+        <QuestionsContainer>
+            <h2>Questions</h2>
+        </QuestionsContainer>
+    );
+}
---
+++ pages/questions/questions.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const QuestionsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin: 5%;
+`;
+
+export { QuestionsContainer };
