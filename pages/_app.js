ff870d6b2863f98168fbb4b3723783627b012fed
Milind452 <mohapatra.milind@gmail.com>
2023-02-21 08:40:07 +0530

    Add global styles to App

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont,
+        "Segoe UI", "Roboto", "Oxygen","Ubuntu",  
+        "Cantarell", "Fira Sans", "Droid Sans",
+        "Helvetica Neue", sans-serif;  
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <GlobalStyle />
+            <Component {...pageProps} />;
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
