8bca3fa1ddea6ccb162b7ca3ff49784201bd9c8d
Milind452 <mohapatra.milind@gmail.com>
2023-02-21 09:18:09 +0530

    Fix document

--- pages/_document.js
+++ pages/_document.js
@@ -6,21 +6,20 @@ export default class MyDocument extends Document {
         const sheet = new ServerStyleSheet();
         const originalRenderPage = ctx.renderPage;
         try {
-            ctx.renderPage = async () => {
+            ctx.renderPage = () =>
                 originalRenderPage({
                     enhanceApp: (App) => (props) =>
                         sheet.collectStyles(<App {...props} />),
                 });
-                const initialProps = await Document.getInitialProps(ctx);
-                return {
-                    ...initialProps,
-                    styles: (
-                        <>
-                            {initialProps.styles}
-                            {sheet.getStyleElement()}
-                        </>
-                    ),
-                };
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
             };
         } catch (error) {
             console.log(error);
