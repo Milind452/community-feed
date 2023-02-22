4619989ca29341231ae188b4a71221c96b3598cf
Milind452 <mohapatra.milind@gmail.com>
2023-02-22 11:45:09 +0530

    use ssr for rendering questions

--- pages/questions/index.js
+++ pages/questions/index.js
@@ -2,66 +2,49 @@ import { QuestionsContainer, CardLink } from "./questions.styles";
 import Card from "../../components/Card/Card";
 import Pagination from "../../components/pagination/Pagination";
 
-import { useState, useEffect } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
-export default function Questions() {
-    const [loading, setLoading] = useState(false);
-    const [questions, setQuestions] = useState([]);
-    const [hasMore, setHasMore] = useState(false);
-
-    const router = useRouter();
-    const { page } = router.query;
-
-    useEffect(() => {
-        async function fetchData() {
-            const data = await fetch(
-                `https://api.stackexchange.com/2.2/questions?${
-                    page ? `page=${page}&` : ""
-                }order=desc&sort=hot&tagged=reactjs&site=stackoverflow`
-            );
-            const result = await data.json();
-            if (result) {
-                setQuestions(result.items);
-                setHasMore(result.has_more);
-                setLoading(false);
-            }
-        }
-        setLoading(true);
-        fetchData();
-    }, [page]);
-
+export default function Questions({ questions, hasMore, page }) {
     return (
         <QuestionsContainer>
             <h2>Questions</h2>
-            {loading ? (
-                <span>loading...</span>
-            ) : (
-                <>
-                    <div>
-                        {questions.map((question) => (
-                            <Link
-                                key={question.question_id}
-                                href={`/questions/${question.question_id}`}
-                                passHref
-                            >
-                                <CardLink>
-                                    <Card
-                                        title={question.title}
-                                        views={question.view_count}
-                                        answers={question.answer_count}
-                                    />
-                                </CardLink>
-                            </Link>
-                        ))}
-                    </div>
-                    <Pagination
-                        currentPage={parseInt(page) || 1}
-                        hasMore={hasMore}
-                    />
-                </>
-            )}
+            <div>
+                {questions &&
+                    questions.map((question) => (
+                        <Link
+                            key={question.question_id}
+                            href={`/questions/${question.question_id}`}
+                            passHref
+                        >
+                            <CardLink>
+                                <Card
+                                    title={question.title}
+                                    views={question.view_count}
+                                    answers={question.answer_count}
+                                />
+                            </CardLink>
+                        </Link>
+                    ))}
+            </div>
+            <Pagination currentPage={parseInt(page) || 1} hasMore={hasMore} />
         </QuestionsContainer>
     );
+}
+
+export async function getServerSideProps(context) {
+    const { page } = context.query;
+    const data = await fetch(
+        `https://api.stackexchange.com/2.2/questions?${
+            page ? `page=${page}&` : ""
+        }order=desc&sort=hot&tagged=reactjs&site=stackoverflow`
+    );
+    const result = await data.json();
+
+    return {
+        props: {
+            questions: result.items,
+            hasMore: result.has_more,
+            page: page || 1,
+        },
+    };
 }
