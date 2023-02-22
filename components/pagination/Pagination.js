57d70b2e642f6dc53f6d7a3f4253b34019c78037
Milind452 <mohapatra.milind@gmail.com>
2023-02-22 11:08:15 +0530

    Dynamically update prev and next btns

--- components/pagination/Pagination.js
+++ components/pagination/Pagination.js
@@ -6,12 +6,12 @@ export default function Pagination({ currentPage, hasMore }) {
     return (
         <PaginationContainer>
             <Link href={`?page=${parseInt(currentPage) - 1}`}>
-                <PaginationLink>
+                <PaginationLink disabled={currentPage <= 1}>
                     Previous
                 </PaginationLink>
             </Link>
             <Link href={`?page=${parseInt(currentPage) + 1}`}>
-                <PaginationLink>Next</PaginationLink>
+                <PaginationLink disabled={!hasMore}>Next</PaginationLink>
             </Link>
         </PaginationContainer>
     );
--- components/pagination/Pagination.styles.js
+++ components/pagination/Pagination.styles.js
@@ -8,9 +8,10 @@ const PaginationContainer = styled.div`
 const PaginationLink = styled.a`
     padding: 2%;
     margin: 1%;
-    background: orange;
     color: white;
-    cursor: pointer;
+    background: ${(props) => (props.disabled ? "lightgrey" : "orange")};
+    pointer-events: ${(props) => (props.disabled ? "none" : "all")};
+    cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
     text-decoration: none;
     border-radius: 5px;
 `;
