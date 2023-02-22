793b15ffa793f9d54000b62e455a36dc4affc54e
Milind452 <mohapatra.milind@gmail.com>
2023-02-22 15:26:11 +0530

    Add ssr to root route

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-import Questions from "./questions";
+import Questions, {
+    getServerSideProps as getServerSideProps_Questions,
+} from "./questions";
 
-export default function Home() {
-    return <Questions />;
+export function getServerSideProps(context) {
+    return getServerSideProps_Questions(context);
+}
+
+export default function Home(props) {
+    return <Questions {...props} />;
 }
