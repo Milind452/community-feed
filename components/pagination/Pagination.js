45d66f7fd73c2cd53bebdfd10734eecc69b11393
Milind452 <mohapatra.milind@gmail.com>
2023-02-22 11:02:00 +0530

    Fix link href

--- components/pagination/Pagination.js
+++ components/pagination/Pagination.js
@@ -5,10 +5,12 @@ import Link from "next/link";
 export default function Pagination({ currentPage, hasMore }) {
     return (
         <PaginationContainer>
-            <Link href={`page=${parseInt(currentPage) - 1}`}>
-                <PaginationLink>Previous</PaginationLink>
+            <Link href={`?page=${parseInt(currentPage) - 1}`}>
+                <PaginationLink>
+                    Previous
+                </PaginationLink>
             </Link>
-            <Link href={`page=${parseInt(currentPage) + 1}`}>
+            <Link href={`?page=${parseInt(currentPage) + 1}`}>
                 <PaginationLink>Next</PaginationLink>
             </Link>
         </PaginationContainer>
